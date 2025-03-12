70c45e3e8a8de9dfa201d617555ee82c2ae4008a
strive <2205487298@qq.com>
2025-03-12 22:04:02 +0800

    initial

---
+++ Game/src/main/java/com/strive/GoldMiner/GameWin.java
@@ -0,0 +1,27 @@
+package com.strive.GoldMiner;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWin extends JFrame {
+
+    Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/com/strive/GoldMiner/gold.png"));
+
+    void launch() {
+        this.setVisible(true);
+        this.setSize(1200, 800);
+        this.setLocationRelativeTo(null);
+        this.setTitle("MY GAME");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage(image, 0, 0, null);
+    }
+
+    public static void main(String[] args) {
+        GameWin gw = new GameWin();
+        gw.launch();
+    }
+}
