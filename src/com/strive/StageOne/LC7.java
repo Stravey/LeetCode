1616b68993e0dd1c3326403668b69a2d7d98ba1e
strive <2205487298@qq.com>
2025-03-26 20:31:06 +0800

    update

--- src/com/strive/StageOne/LC7.java
+++ src/com/strive/StageOne/LC7.java
@@ -5,13 +5,13 @@ public class LC7 {
         int sum = 0;
         while(x != 0) {
             int temp = x % 10;
-            if (sum > 214748364 || (sum == 214748364 && temp>7)) {
+            if (sum > 214748364 || (sum == 214748364 && temp > 7)) {
                 return 0;
             }
-            if (sum <- 214748364 || (sum == -214748364 && temp<-8)) {
+            if (sum < -214748364 || (sum == -214748364 && temp < -8)) {
                 return 0;
             }
-            sum = sum *10 + temp;
+            sum = sum * 10 + temp;
             x /= 10;
         }
         return sum;
@@ -20,5 +20,6 @@ public class LC7 {
     public static void main(String[] args) {
         LC7 lc = new LC7();
         System.out.println(lc.reverse(12412));
+        System.out.println(lc.reverse(-12412));
     }
 }
