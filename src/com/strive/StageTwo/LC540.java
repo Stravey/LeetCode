59dfb73d375a872ec225a54a6368419f5ae3bf04
strive <2205487298@qq.com>
2025-04-14 11:11:10 +0800

    update LC540

---
+++ src/com/strive/StageTwo/LC540.java
@@ -0,0 +1,18 @@
+package com.strive.StageTwo;
+
+public class LC540 {
+    public int singleNonDuplicate(int[] nums) {
+        for(int i = 1;i < nums.length - 1;i++) {
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
+                return nums[i];
+            }
+        }
+        if(nums.length == 1 ) {
+            return nums[0];
+        }
+        if(nums[0] != nums[1]) {
+            return nums[0];
+        }
+        return nums[nums.length-1];
+    }
+}
